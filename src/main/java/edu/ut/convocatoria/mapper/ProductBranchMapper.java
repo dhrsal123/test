b1a9acf814e1d3e096d885957576a424af283786
Santiago Gonzalez Diaz <rstdtas@proton.me>
2026-06-03 02:07:26 -0400

    feat: update mappers

--- src/main/java/edu/ut/convocatoria/mapper/ProductBranchMapper.java
+++ src/main/java/edu/ut/convocatoria/mapper/ProductBranchMapper.java
@@ -1,12 +1,18 @@
 package edu.ut.convocatoria.mapper;
 
+import edu.ut.convocatoria.domain.dto.response.MaxStockResponseDTO;
 import edu.ut.convocatoria.domain.dto.response.ProductStockResponseDTO;
+import edu.ut.convocatoria.domain.entity.MaxStockProductProjection;
 import edu.ut.convocatoria.domain.entity.ProductEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING
+)
 public interface ProductBranchMapper {
 
     @Mapping(target = "name", source = "productEntity.name")
@@ -14,4 +20,11 @@ public interface ProductBranchMapper {
     @Mapping(target = "price", source = "productEntity.price")
     @Mapping(target = "stock", source = "stock")
     ProductStockResponseDTO toDTO(ProductEntity productEntity, Integer stock);
+
+    @Mapping(target = "branchId", source = "branchId")
+    @Mapping(target = "maxStockProduct.id", source = "productId")
+    @Mapping(target = "maxStockProduct.name", source = "name")
+    @Mapping(target = "maxStockProduct.description", source = "description")
+    @Mapping(target = "maxStockProduct.price", source = "price")
+    MaxStockResponseDTO toMaxStockDto(MaxStockProductProjection projection);
 }
--- src/main/java/edu/ut/convocatoria/mapper/ProductMapper.java
+++ src/main/java/edu/ut/convocatoria/mapper/ProductMapper.java
@@ -5,8 +5,12 @@ import edu.ut.convocatoria.domain.dto.response.ProductResponseDTO;
 import edu.ut.convocatoria.domain.entity.ProductEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING
+)
 public interface ProductMapper {
     ProductEntity toEntity(ProductRequestDTO productRequestDTO);
 
