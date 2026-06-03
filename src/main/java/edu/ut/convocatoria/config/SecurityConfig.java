a2faac11a73ce4071e2ecb5627efcf9ebef2d00b
Santiago Gonzalez Diaz <rstdtas@proton.me>
2026-06-03 12:28:26 -0400

    feat: Allow swagger without auth

--- src/main/java/edu/ut/convocatoria/config/SecurityConfig.java
+++ src/main/java/edu/ut/convocatoria/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
                                 .requestMatchers("/api/v1/auth/register").permitAll()
                                 .requestMatchers("/api/v1/auth/login").permitAll()
                                 .requestMatchers("/swagger-ui**").permitAll()
+                                .requestMatchers("/swagger-ui/**").permitAll()
                                 .requestMatchers("/v3/api-docs/**").permitAll()
                                 .requestMatchers("/swagger/v3/api-docs/**").permitAll()
                                 .requestMatchers("/swagger-ui/swagger-config**").permitAll()
