4e8074be38a72321a807c3a2de64c000d523b720
Santiago Gonzalez Diaz <rstdtas@proton.me>
2026-06-03 02:08:13 -0400

    feat: add max stock projection

---
+++ src/main/java/edu/ut/convocatoria/domain/entity/MaxStockProductProjection.java
@@ -0,0 +1,21 @@
+package edu.ut.convocatoria.domain.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MaxStockProductProjection {
+    private UUID branchId;
+    private UUID productId;
+    private String name;
+    private String description;
+    private Double price;
+}
