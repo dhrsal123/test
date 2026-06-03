eb71692c55c6dfbd4629fc57c00e69fc6b63e84c
Santiago Gonzalez Diaz <rstdtas@proton.me>
2026-06-03 01:17:09 -0400

    feat: add id's to dto's

--- src/main/java/edu/ut/convocatoria/domain/dto/response/ProductResponseDTO.java
+++ src/main/java/edu/ut/convocatoria/domain/dto/response/ProductResponseDTO.java
@@ -1,7 +1,10 @@
 package edu.ut.convocatoria.domain.dto.response;
 
 
+import java.util.UUID;
+
 public record ProductResponseDTO(
+        UUID id,
         String name,
         String description,
         Double price
--- src/main/java/edu/ut/convocatoria/domain/dto/response/ProductStockResponseDTO.java
+++ src/main/java/edu/ut/convocatoria/domain/dto/response/ProductStockResponseDTO.java
@@ -1,6 +1,9 @@
 package edu.ut.convocatoria.domain.dto.response;
 
+import java.util.UUID;
+
 public record ProductStockResponseDTO(
+        UUID id,
         String name,
         String description,
         Double price,
